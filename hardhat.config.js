ce68baae7cf708eee1aed6cf2af274afa7799655
Dokkai-B <xix.carlaguas.xix@gmail.com>
2024-07-13 23:13:54 +0800

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,15 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+
+// Replace with your private key
+const PRIVATE_KEY = '59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d';
 
 module.exports = {
   solidity: "0.8.17",
   networks: {
     localhost: {
       url: "http://127.0.0.1:8545",
-    },
-  },
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  }
 };
